ac8e097cc96d04bf17504c7136073584660a8125
Pawel Sokolowski <pfalkon@gmail.com>
2016-11-19 13:40:34 +0100

    Hero search component

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,12 @@ import {FormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
 import {HeroDetailComponent} from "./hero-detail.component";
 import {HeroesComponent} from "./heroes.component";
+import {HeroSearchComponent} from "./hero-search.component";
 import {HeroService} from "./hero.service";
 import {DashboardComponent} from "./dashboard.component";
 import {AppRoutingModule} from "./app-routing.module";
 import {HttpModule} from "@angular/http";
+import "./rxjs-extensions";
 // Imports for loading & configuring the in-memory web api
 import {InMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "./in-memory-data.service";
@@ -26,6 +28,7 @@ import {InMemoryDataService} from "./in-memory-data.service";
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
+    HeroSearchComponent,
   ],
   providers: [
     HeroService,
